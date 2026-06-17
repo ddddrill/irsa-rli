b5b4127db5c3474db45bf7806973881cd4a03346
ddddrill <ddddrill@users.noreply.github.com>
2026-06-17 20:03:41 +0300

    chore: merge build.py into build.bat, remove extra file

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 @echo off
 echo === Building IRSA ===
-python build.py
+
+set PYINSTALLER_ISOLATED_BUILD=0
+if exist dist rmdir /s /q dist
+if exist build rmdir /s /q build
+
+.build_venv\Scripts\python.exe -m PyInstaller IRSA.spec --noconfirm
+
+echo === Done: dist\IRSA\IRSA.exe ===
 pause
